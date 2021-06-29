3515bd75dd3f76c1c43383fdf6cc029a049289dc
seankuendig <18509274+seankuendig@users.noreply.github.com>
2021-06-29 13:46:35 +0200

    testing

--- lb2/js/index.js
+++ lb2/js/index.js
@@ -1,6 +1,19 @@
-const videoTag = document.getElementById("video_tag");
+function playVideo() {
+    console.log('asdfasdf')
+    const videoTag = document.getElementById("video_tag");
 
-const mediaSource = new MediaSource();
-const url = URL.createObjectURL(mediaSource);
+    const mediaSource = new MediaSource();
+    const url = URL.createObjectURL(mediaSource);
 
-videoTag.src = url;
+    videoTag.src = url;
+
+    const videoSourceBuffer = mediaSource.addSourceBuffer('video/mp4; codecs="avc1.4d400d,mp4a.40.2"');
+
+    // the same for the video SourceBuffer
+    fetch("../sample-mp4-file.mp4").then(function (response) {
+        // The data has to be a JavaScript ArrayBuffer
+        return response.arrayBuffer();
+    }).then(function (videoData) {
+        videoSourceBuffer.appendBuffer(videoData);
+    });
+}
